650f5f99bae713015a6b2c1d3ed5f130255f1a12
Richard Walker <well@thatisrich.co.uk>
2024-07-08 14:16:28 +0100

    chore: setting up PHP Unit tests

---
+++ tests/phpunit/tests/class-phpunit-operational-test.php
@@ -0,0 +1,16 @@
+<?php
+
+declare( strict_types = 1 );
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test that PHPUnit is functioning correctly
+ */
+class PHPUnit_Operational_Test extends TestCase {
+
+	public function test_operational() {
+		$this->assertTrue( true );
+	}
+
+}
---
+++ tests/phpunit/tests/class-rest-api-test.php
@@ -0,0 +1,11 @@
+<?php
+
+declare( strict_types = 1 );
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Rest_Api tests will be added here
+ */
+class Rest_Api_Test extends TestCase {
+}
