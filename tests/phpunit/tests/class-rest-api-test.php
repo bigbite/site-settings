8755d8fcae20ac24ef169b071f2bea39f4d9fcd2
Richard Walker <well@thatisrich.co.uk>
2024-07-08 14:26:20 +0100

    chore: adding basic assetion tests for methods

--- tests/phpunit/tests/class-rest-api-test.php
+++ tests/phpunit/tests/class-rest-api-test.php
@@ -3,9 +3,19 @@
 declare( strict_types = 1 );
 
 use PHPUnit\Framework\TestCase;
+use Big_Bite\Site_Settings\Rest_Api;
 
 /**
  * Rest_Api tests will be added here
  */
 class Rest_Api_Test extends TestCase {
+
+	public function test_get_value(): void {
+		$this->assertTrue( true );
+    }
+
+	public function test_get_attributes(): void {
+		$this->assertTrue( true );
+    }
+
 }
