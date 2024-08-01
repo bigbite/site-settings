7fc5c0b0e4b5f4f44349e8198a282e5ee1543ead
Dom Waterson <dom_waterson@hotmail.com>
2024-08-01 14:52:37 +0100

    refactor: updated checkbox group to include the name attribute

--- src/admin/fields/CheckboxGroup.js
+++ src/admin/fields/CheckboxGroup.js
@@ -1,6 +1,19 @@
 import { CheckboxControl } from '@wordpress/components';
 
 const CheckboxGroup = ( { label, options, onChange, ...rest } ) => {
+	/**
+	 * Updates the checked value of an option and calls
+	 * the onChange callback with new options
+	 *
+	 * @param {boolean} value       - checked value
+	 * @param {number}  optionIndex - index of the option
+	 */
+	function handleCheckboxChange( value, optionIndex ) {
+		const newOptions = [ ...options ];
+		newOptions[ optionIndex ].checked = value;
+		onChange( newOptions );
+	}
+
 	return (
 		<fieldset>
 			<legend className="checkbox-group__label">{ label }</legend>
@@ -9,11 +22,10 @@ const CheckboxGroup = ( { label, options, onChange, ...rest } ) => {
 					key={ option.id }
 					label={ option.label }
 					checked={ option.checked }
-					onChange={ ( value ) => {
-						const newOptions = [ ...options ];
-						newOptions[ index ].checked = value;
-						onChange( newOptions );
-					} }
+					name={ label.toLowerCase().replace( /\s/g, '-' ) }
+					onChange={ ( value ) =>
+						handleCheckboxChange( value, index )
+					}
 					{ ...rest }
 				/>
 			) ) }
