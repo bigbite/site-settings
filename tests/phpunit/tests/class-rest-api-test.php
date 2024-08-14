f6d8ce66a58073b8367213c7f3856be0b61b0814
Richard Walker <well@thatisrich.co.uk>
2024-08-14 12:42:09 +0100

    fix: refactoring tests to use BrainMonkey

--- tests/phpunit/tests/class-rest-api-test.php
+++ tests/phpunit/tests/class-rest-api-test.php
@@ -4,6 +4,7 @@ declare( strict_types = 1 );
 
 use PHPUnit\Framework\TestCase;
 use Big_Bite\Site_Settings\Rest_Api;
+use Brain\Monkey;
 
 /**
  * Rest_Api tests for get_value and get_attributes methods.
@@ -11,137 +12,81 @@ use Big_Bite\Site_Settings\Rest_Api;
 class Rest_Api_Test extends TestCase {
 
 	/**
-     * Provides dummy data which follows the DB structure for testing.
-     *
-     * @return array
-     */
-    private function getDummyData() {
-        return [
-            'general' => [
-                [
-                    'field' => 'radio',
-                    'value' => 2,
-                    'id' => '187891723-123y8i21',
-                    'attributes' => [
-                        'label' => 'Radio Label',
-                        'selected' => 2,
-                        'options' => [
-                            ['label' => 'Option 1', 'value' => 1],
-                            ['label' => 'Option 2', 'value' => 2],
-                            ['label' => 'Option 3', 'value' => 3],
-                        ],
-                    ],
-                ],
-                [
-                    'field' => 'text',
-                    'value' => 'hello world',
-                    'id' => '12312312-12323',
-                    'attributes' => [
-                        'label' => 'Text Label',
-                        'value' => 'hello world',
-                    ],
-                ],
-            ],
-            'analytics' => [
-                [
-                    'field' => 'checkbox-group',
-                    'value' => ['Post 1', 'Post 2'],
-                    'id' => '1121212121',
-                    'attributes' => [
-                        'label' => 'New Setting for checkbox group',
-                        'options' => [
-                            ['label' => 'Post 1', 'checked' => true],
-                            ['label' => 'Post 2', 'checked' => true],
-                            ['label' => 'Post 3', 'checked' => false],
-                        ],
-                    ],
-                ],
-                [
-                    'field' => 'toggle',
-                    'value' => true,
-                    'id' => '123123123-123123',
-                    'attributes' => [
-                        'label' => 'Toggle Label',
-                        'value' => true,
-                    ],
-                ],
-            ],
-            'styles' => [],
-		];
+	 * Define private variable for the Rest_Api class
+	 */
+	private $restApi;
+
+	/**
+	 * Run before each test.
+	 *
+	 * Stub the get_option function to return an example JSON string.
+	 */
+	protected function setUp(): void {
+		parent::setUp();
+		Monkey\setUp();
+		Monkey\Functions\stubs(
+			[
+				'get_option' => '{"general":[{"field":"text","attributes":{"label":"Text Label","value":"API Testing"},"value":"API Testing","id":"1234567890"}],"analytics":[{"field":"text","value":null,"id":"0987654321"}]}',
+			]
+		);
+
+		// Instantiate the Rest_Api class
+		$this->restApi = new Rest_Api();
+	}
+
+	/**
+	 * Run after each test to clean up
+	 */
+	protected function tearDown() : void {
+		Monkey\tearDown();
+		parent::tearDown();
 	}
 
 	/**
 	 * Test for get_value method to ensure return value is correct.
-	 *
-	 * NOTE - Dummy data for initial structure only
 	 */
 	public function test_get_value(): void {
+		// Define testing variables
+		$test_category = 'general';
+		$test_value_ID = '1234567890';
+		$test_expected_value = 'API Testing';
 
-		// Define expected testing values
-		$expected_category = 'general';
-		$expected_ID = '12312312-12323';
-		$expected_value = 'hello world';
+		// Test a null return if category doesn't exist
+		$this->assertNull($this->restApi->get_value('failedCategory', $test_value_ID));
 
-		$dummy_data = $this->getDummyData();
+		// Test a null return if the ID doesn't exist
+		$this->assertNull($this->restApi->get_value($test_category, 'incorrect-ID'));
 
-        // Ensure the data is an array
-        $this->assertIsArray($dummy_data);
+		// Test the returned value is a string
+		$this->assertIsString($this->restApi->get_value($test_category, $test_value_ID));
 
-		// Check to ensure the data has the category
-		$this->assertArrayHasKey($expected_category, $dummy_data);
+		// Test when the value does not exist within a category
+		$this->assertNull($this->restApi->get_value('analytics', '0987654321'));
 
-		// Set the category specific data
-		$category_data = $dummy_data[$expected_category];
-
-		// Loop through the target category data to find the expected ID
-		foreach ( $category_data as $item ) {
-			if ( $item['id'] === $expected_ID ) {
-				// Set the dummp value ready to test
-				$dummy_value = $item['value'];
-				break;
-			}
-		}
-
-		// Then check to ensure the ID exists and returns the right value
-		$this->assertSame( $expected_value, $dummy_value );
-
-    }
+		// Test the returned value is equal to the expected value
+		$this->assertEquals($test_expected_value, $this->restApi->get_value($test_category, $test_value_ID));
+	}
 
 	/**
-	 * Test for get_attributes method to ensure return value is correct.
-	 *
-	 * NOTE - Dummy data for initial structure only
+	 * Test for get_value method to ensure return value is correct.
 	 */
 	public function test_get_attributes(): void {
+		// Define testing variables
+		$test_category = 'general';
+		$test_value_ID = '1234567890';
+		$test_expected_value = 'API Testing';
 
-		// Define expected testing values
-		$expected_category = 'analytics';
-		$expected_ID = '123123123-123123';
-		$expected_attr = ['label' => 'Toggle Label', 'value' => true];
+		// Test a null return if category doesn't exist
+		$this->assertNull($this->restApi->get_attributes('failedCategory', $test_value_ID));
 
-		$dummy_data = $this->getDummyData();
+		// Test a null return if the ID doesn't exist
+		$this->assertNull($this->restApi->get_attributes($test_category, 'incorrect-ID'));
 
-        // Ensure the data is an array
-        $this->assertIsArray($dummy_data);
+		// Test the returned value is an array
+		$this->assertIsArray($this->restApi->get_attributes($test_category, $test_value_ID));
 
-		// Check to ensure the data has the category
-		$this->assertArrayHasKey('analytics', $dummy_data);
-
-		// Set the category specific data
-		$category_data = $dummy_data[$expected_category];
-
-		// Loop through the target category data to find the expected ID
-		foreach ( $category_data as $item ) {
-			if ( $item['id'] === $expected_ID ) {
-				// Set the dummp value ready to test
-				$dummy_attr = $item['attributes'];
-				break;
-			}
-		}
-
-		// Then check to ensure the ID exists and returns the right attributes
-		$this->assertSame( $expected_attr, $dummy_attr );
-
-    }
+		// Test null return if the attributes do not exist
+		$this->assertNull($this->restApi->get_value('analytics', '0987654321'));
+	}
 
 }
